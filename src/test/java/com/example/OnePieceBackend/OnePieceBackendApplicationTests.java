9bbff6cdde5b0d981cd7f0e0dc9767d2adae13d9
Amman Nega <ammannega@gmail.com>
2024-09-10 17:57:06 -0700

    Initial commit with Spring Boot boilerplate code

---
+++ src/test/java/com/example/OnePieceBackend/OnePieceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OnePieceBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePieceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
