e6c9083730eeb71b2a94f60a360df85305d814d4
Amman Nega <ammannega@gmail.com>
2024-10-13 01:18:28 -0700

    created Crew bean class

---
+++ src/main/java/com/example/OnePieceBackend/crew/Crew.java
@@ -0,0 +1,89 @@
+package com.example.OnePieceBackend.crew;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Crew {
+    @Id
+    @SequenceGenerator(
+        name = "crew_sequence",
+        sequenceName = "crew_sequence",
+        allocationSize = 1 
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "crew_sequence"
+    )
+    private Long id;
+    private String name;
+    
+    public Crew() {
+    }
+
+    public Crew(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Crew(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Crew [id=" + id + ", name=" + name + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Crew other = (Crew) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+}
