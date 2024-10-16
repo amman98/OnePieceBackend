1ae3d584bd9d1166fef90d20516c423b4ba9964e
Amman Nega <ammannega@gmail.com>
2024-10-15 19:43:05 -0700

    added API endpoints to controller class and test values in config

--- src/main/java/com/example/OnePieceBackend/crew/CrewConfig.java
+++ src/main/java/com/example/OnePieceBackend/crew/CrewConfig.java
@@ -1,5 +1,29 @@
 package com.example.OnePieceBackend.crew;
 
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class CrewConfig {
     
+    @Bean
+    CommandLineRunner crewCommandLineRunner(
+            CrewRepository repository) {
+        return args -> {
+            Crew strawHats = new Crew(
+                "Straw Hat Pirates"
+            );
+
+            Crew redHair = new Crew(
+                "Red Hair Pirates"
+            );
+
+            repository.saveAll(
+                List.of(strawHats, redHair)
+            );
+        };
+    }
 }
--- src/main/java/com/example/OnePieceBackend/crew/CrewController.java
+++ src/main/java/com/example/OnePieceBackend/crew/CrewController.java
@@ -1,5 +1,54 @@
 package com.example.OnePieceBackend.crew;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/crew")
 public class CrewController {
-    
+    private final CrewService crewService;
+
+    @Autowired
+    public CrewController(CrewService crewService) {
+        this.crewService = crewService;
+    }
+
+    @GetMapping(path = "{crewId}")
+    public Optional<Crew> getCrew(@PathVariable("crewId") Long crewId) {
+        return crewService.getCrew(crewId);
+    }
+
+    @GetMapping
+    public List<Crew> getAllCrews() {
+        return crewService.getAllCrews();
+    }
+
+    @PostMapping
+    public void addNewCrew(@RequestBody Crew crew) {
+        crewService.addNewCrew(crew);
+    }
+
+    @DeleteMapping(path = "{crewId}")
+    public void deleteCrew(
+            @PathVariable("crewId") Long crewId) {
+        crewService.deleteCrew(crewId);
+    }
+
+    @PutMapping(path = "{crewId}")
+    public void updateCrew(
+            @PathVariable("crewId") Long crewId, 
+            @RequestParam(required = false) String name) {
+        crewService.updateCrew(crewId, name);
+    }
 }
